594df36667dabb16166211d7d0524bb303271585
1VishalSD <ilearnvk@gmail.com>
2024-10-24 14:28:00 +0530

    make the navbar path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-import "./App.css";
 import { Outlet } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>Navbar</div>
+      <Navbar />
       <Outlet />
     </>
   );
