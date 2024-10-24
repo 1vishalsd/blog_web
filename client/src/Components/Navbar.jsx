32a63ce73a9f397b118a4ebd04bf1380b1c4a1a9
1VishalSD <ilearnvk@gmail.com>
2024-10-24 15:19:18 +0530

    make the our navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,16 +1,46 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const navItems = [
-    { path: "/", Link: "Home" }
-    { path: "/about", Link: "About" }
-    { path: "/services", Link: "Service" }
-    { path: "/contact", Link: "Contact" }
-    { path: "/blogs", Link: "Blog" }
-
+    {
+      path: "/",
+      label: "Home",
+    },
+    {
+      path: "/blogs",
+      label: "Blog",
+    },
+    {
+      path: "/about",
+      label: "About",
+    },
+    {
+      path: "/services",
+      label: "Service",
+    },
+    {
+      path: "/contact",
+      label: "Contact",
+    },
   ];
-  return <div>Navbar</div>;
+
+  return (
+    <header className="bg-black">
+      <nav className="p-4">
+        <a href="/" className="text-xl font-bold text-white">
+          Design <span className="text-blue-500">VK</span>
+        </a>
+        <ul className=" md:flex gap-12 text-lg hidden">
+          {navItems.map((item, index) => (
+            <li key={index} className="text-white">
+              <NavLink to={item.path}>{item.label}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
